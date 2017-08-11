407347f674f60ef0018f5defa512ac02db637e17
Ce Gao <ce.gao@outlook.com>
2017-08-11 10:37:57 +0800

    scripts: Keep the .git in docs
    Signed-off-by: Ce Gao <ce.gao@outlook.com>

--- scripts/generate-reference.sh
+++ scripts/generate-reference.sh
@@ -3,8 +3,10 @@
 ROOT=$(dirname "${BASH_SOURCE}")/..
 
 cd ${ROOT}
-rm -rf docs
+mv docs/.git cache.git.dir
+rm -rf docs/
 mkdir -p docs/reference
+mv cache.git.dir docs/.git
 cp -r content/* docs/
 exec python3 ./src/generate.py $@
 cd - > /dev/null
